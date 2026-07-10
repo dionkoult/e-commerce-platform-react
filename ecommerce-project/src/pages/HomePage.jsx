503b4f36f478aba2bf50c0a6a39cbbb6096e5a5c
Dionysis <dionusiskoultouras@gmail.com>
2026-07-10 22:55:14 +0300

    Use data from the backend to generate the HTML

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,19 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './HomePage.css';
 import { Header } from '../components/Header';
 import checkMark from '../assets/images/icons/checkmark.png';
 import { products } from '../../starting-code/data/products';
 
 export function HomePage() {
-  axios.get('http://localhost:3000/api/products')
-    .then((response) => {
-      console.log(response.data);
-    });
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+      .then((response) => {
+        setProducts(response.data);
+      });
+  }, []);
 
   return (
     <>
