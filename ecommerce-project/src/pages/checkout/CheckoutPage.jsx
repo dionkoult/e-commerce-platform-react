d064eedeaee6c60ee1aaa906373dd4cfc523f4ff
Dionysis <dionusiskoultouras@gmail.com>
2026-07-14 13:35:19 +0300

    Use seperate use effect for payment summary

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -11,14 +11,20 @@ export function CheckoutPage({ cart, loadCart }) {
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
+      const response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
       setDeliveryOptions(response.data);
+    };
 
-      response = await axios.get('/api/payment-summary');
+    fetchCheckoutData();
+  }, []);
+
+  useEffect(() => {
+    const reloadPaymentSummary = async () => {
+      const response = await axios.get('/api/payment-summary');
       setPaymentSummary(response.data);
     };
-   
-    fetchCheckoutData();  
+
+    reloadPaymentSummary();
   }, [cart]);
 
   return (
