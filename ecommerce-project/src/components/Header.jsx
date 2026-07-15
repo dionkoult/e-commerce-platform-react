ffb742f6187d785a3170518801f994ce480972f5
Dionysis <dionusiskoultouras@gmail.com>
2026-07-16 00:15:32 +0300

    Search bar - part 1

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -4,9 +4,18 @@ import logoWhite from "../assets/images/logo-white.png";
 import mobileLogoWhite from "../assets/images/mobile-logo-white.png";
 import searchIcon from '../assets/images/icons/search-icon.png';
 import cartIcon from '../assets/images/icons/cart-icon.png';
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
 export function Header({ cart }) {
+  const [ searchInput, setSearchInput ] = useState(null);
+
+  const navigate = useNavigate();
+
+  function updateSearchInput(event) {
+    setSearchInput(event.target.value);
+  }
+
   let totalQuantity = 0;
 
   cart.forEach((cartItem) => {
@@ -25,9 +34,17 @@ export function Header({ cart }) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input className="search-bar"
+          type="text"
+          placeholder="Search"
+          onChange={updateSearchInput}
+          />
 
-        <button className="search-button">
+        <button className="search-button"
+          onClick={() => {
+            navigate(`/?search=${searchInput}`);
+          }}
+        >
           <img className="search-icon" src={searchIcon} />
         </button>
       </div>
