7af58491e1526666f4f0b0a79fd5c4f12456ab64
Dionysis <dionusiskoultouras@gmail.com>
2026-07-16 23:54:02 +0300

    PaymentSummary - part 1

--- ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
+++ ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
@@ -2,5 +2,39 @@ import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import axios from 'axios';
-import { HomePage } from './HomePage';
+import { PaymentSummary } from './PaymentSummary';
 import userEvent from '@testing-library/user-event';
+
+describe('PaymentSummary component', () => {
+  let loadCart;
+
+  beforeEach(() => {
+    loadCart = vi.fn();
+  });
+
+  it('displays the correct dollar amount', async () => {
+    const sample = await axios.get('http://localhost:3000/api/payment-summary');
+    const sampleSummary = sample.data;
+
+    render(
+      <MemoryRouter>
+        <PaymentSummary loadCart={loadCart} paymentSummary={sampleSummary} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByTestId('payment-summary-row-items'))
+      .toHaveTextContent('Items (6)');
+
+    expect(screen.getByTestId('payment-summary-row-shipping'))
+      .toHaveTextContent('Shipping & handling:$0.00');
+
+    expect(screen.getByTestId('payment-summary-row-total-before-tax'))
+      .toHaveTextContent('Total before tax:$125.70');
+
+    expect(screen.getByTestId('payment-summary-row-estimated-tax'))
+      .toHaveTextContent('Estimated tax (10%):$12.57');
+
+    expect(screen.getByTestId('payment-summary-row-order-total'))
+      .toHaveTextContent('Order total:$138.27');
+  });
+});
