5923149156401397233b76e1e76dbcecca8cc430
Dionysis <dionusiskoultouras@gmail.com>
2026-07-17 23:02:13 +0300

    payment tests - part 2

--- ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
+++ ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
@@ -1,9 +1,8 @@
 import { it, expect, describe, vi, beforeEach } from 'vitest';
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import axios from 'axios';
 import { PaymentSummary } from './PaymentSummary';
-import userEvent from '@testing-library/user-event';
 
 describe('PaymentSummary component', () => {
   let loadCart;
