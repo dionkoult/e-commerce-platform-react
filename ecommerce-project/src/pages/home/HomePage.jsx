b751b2bf83a17be20e368d1c2f0bd54abc5295b6
Dionysis <dionusiskoultouras@gmail.com>
2026-07-20 22:01:07 +0300

    Create no products found message

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -7,23 +7,32 @@ import { useSearchParams } from 'react-router';
 
 export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
+  const [noResults, setNoResults] = useState(false);
 
-  const [ searchParams ] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const search = searchParams.get('search');
 
   useEffect(() => {
     const getHomeData = async () => {
       if (search) {
         const response = await axios.get(`/api/products?search=${search}`);
+        if (response.data.length === 0) {
+          setNoResults(true);
+          setProducts([]);
+          return;
+        }
+
+        setNoResults(false);
         setProducts(response.data);
       } else {
         const response = await axios.get('/api/products');
+        setNoResults(false);
         setProducts(response.data);
       }
     };
 
     getHomeData();
-    }, [search]);
+  }, [search]);
 
   return (
     <>
@@ -32,6 +41,9 @@ export function HomePage({ cart, loadCart }) {
       <Header cart={cart} />
 
       <div className="home-page">
+        {noResults && <div className='no-products-found'>
+          No products matched your search.
+        </div>}
         <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
